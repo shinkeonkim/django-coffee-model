0ba019011732591d7775f593f02bb20254ed37e0
shinkeonkim <singun11@kookmin.ac.kr>
2021-11-14 20:35:00 +0900

    fix: directory structure

--- django_coffee_model/__init__.py
+++ django_coffee_model/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'Django Coffee Model'
-__version__ = '2.1.4'
+__version__ = '2.1.5'
 __author__ = 'shinkeonkim'
 __license__ = 'MIT'
 
--- django_coffee_model/apps.py
+++ django_coffee_model/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CoffeeModelConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "coffee_model"
+    name = "django_coffee_model"
