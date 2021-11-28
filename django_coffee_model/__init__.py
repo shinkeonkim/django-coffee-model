ad0060333512b77b17eaf27a5ae7eb74367f9a5b
shinkeonkim <singun11@kookmin.ac.kr>
2021-11-28 15:32:35 +0900

    feat: versioning

--- django_coffee_model/__init__.py
+++ django_coffee_model/__init__.py
@@ -1,6 +1,6 @@
-__title__ = 'Django Coffee Model'
-__version__ = '2.1.5'
-__author__ = 'shinkeonkim'
-__license__ = 'MIT'
+__title__ = "Django Coffee Model"
+__version__ = "2.1.6"
+__author__ = "shinkeonkim"
+__license__ = "MIT"
 
 VERSION = __version__
